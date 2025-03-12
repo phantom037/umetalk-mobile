8d43f1b80b6abcc723ae67aead22087229c15048
Leo Tran <phantom037>
2025-03-12 17:31:09 +1100

    Fix Android folder structure by removing nested Git repository

---
+++ android/app/src/main/kotlin/com/example/ume_talk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ume_talk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/leotran9x/ume_talk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.leotran9x.ume_talk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
